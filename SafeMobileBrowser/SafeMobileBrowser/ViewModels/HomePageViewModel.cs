dd1b3c286c727ff16be3ab5474b3d21519716594
vigneshwara044 <vigneshwara044@gmail.com>
2019-08-06 11:52:23 +0530

    fix/browser: hide progress indicator during navigation

--- SafeMobileBrowser/SafeMobileBrowser/ViewModels/HomePageViewModel.cs
+++ SafeMobileBrowser/SafeMobileBrowser/ViewModels/HomePageViewModel.cs
@@ -66,10 +66,7 @@ namespace SafeMobileBrowser.ViewModels
         public bool IsNavigating
         {
             get => _isNavigating;
-            set
-            {
-                SetProperty(ref _isNavigating, value);
-            }
+            set => SetProperty(ref _isNavigating, value);
         }
 
         private WebViewSource _url;
@@ -98,6 +95,7 @@ namespace SafeMobileBrowser.ViewModels
                     CurrentUrl = CurrentTitle = $"safe://{value}";
                     CanGoToHomePage = true;
                 }
+
                 CanRefresh = string.Empty.Equals(CurrentUrl) ? false : true;
 
                 OnPropertyChanged(nameof(CanGoToHomePage));
@@ -173,6 +171,7 @@ namespace SafeMobileBrowser.ViewModels
 
             MessagingCenter.Send((App)Application.Current, MessageCenterConstants.UpdateWelcomePageTheme);
             IsNavigating = false;
+            CanRefresh = string.Empty.Equals(CurrentUrl) ? false : true;
         }
 
         private void OnNavigating(WebNavigatingEventArgs args)
@@ -181,6 +180,7 @@ namespace SafeMobileBrowser.ViewModels
             {
                 var url = args.Url;
                 SetAddressBarText(url);
+                CanRefresh = false;
 
                 if (!IsNavigating)
                     IsNavigating = true;
