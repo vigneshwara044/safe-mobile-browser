c8eaac6df59f188c50fde419ceb7d422855cda2e
vigneshwara044 <vigneshwara044@gmail.com>
2019-09-09 22:37:46 +0530

    Settings page test

--- SafeMobileBrowser.UITests/Tests.cs
+++ SafeMobileBrowser.UITests/Tests.cs
@@ -14,6 +14,12 @@ namespace SafeMobileBrowser.UITests
 
         static readonly Func<AppQuery, AppQuery> AddressBarEntry = c => c.Marked("AddressBarEntry");
         static readonly Func<AppQuery, AppQuery> FocusActionIcon = c => c.Marked("FocusActionIcon");
+        static readonly Func<AppQuery, AppQuery> MenuActionIcon = c => c.Marked("MenuActionIcon");
+        static readonly Func<AppQuery, AppQuery> SettingsPageIcon = c => c.Marked("NoResourceEntry-24");
+        static readonly Func<AppQuery, AppQuery> AppLogsLabel = c => c.Marked("NoResourceEntry - 37");
+        static readonly Func<AppQuery, AppQuery> DeleteAllAppLogsIcon = c => c.Id("Delete All logs");
+        static readonly Func<AppQuery, AppQuery> FaqLabel = c => c.Marked("NoResourceEntry-39");
+        static readonly Func<AppQuery, AppQuery> PrivacyStatementLabel = c => c.Marked("NoResourceEntry-40");
 
         public Tests(Platform platform)
         {
@@ -32,6 +38,28 @@ namespace SafeMobileBrowser.UITests
             app.Tap(FocusActionIcon);
             AppResult[] result1 = app.Query(AddressBarEntry);
             app.EnterText(AddressBarEntry, "hello");
+        }
+
+        [Test]
+        public void SettingsPageTest()
+        {
+            app.Tap(MenuActionIcon);
+            app.Repl();
+
+            // Opening Settings Page
+            app.Tap(SettingsPageIcon);
+            app.Tap(FaqLabel);
+        }
+
+        [Test]
+        public void DeleteApplogsTest()
+        {
+            app.Tap(MenuActionIcon);
+            app.Tap(SettingsPageIcon);
+            app.Tap(AppLogsLabel);
+            app.Repl();
+            app.Tap(DeleteAllAppLogsIcon);
+            app.Screenshot("Deleted app logs");
         }
     }
 }
